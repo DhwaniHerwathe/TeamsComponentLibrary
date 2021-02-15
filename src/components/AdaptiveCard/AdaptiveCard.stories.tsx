380db61692e7a7659d965f31f77b79057ea15ad3
Saritha Sreekumar <saritha.sreekumar@accenture.com>
2021-02-15 13:41:51 +0530

    new changes

--- src/components/AdaptiveCard/AdaptiveCard.stories.tsx
+++ src/components/AdaptiveCard/AdaptiveCard.stories.tsx
@@ -22,18 +22,18 @@ const Template: Story<IAdaptiveCardProps> = (args) => <AdaptiveCard {...args} />
 
 export const Default = Template.bind({});
 Default.args = {
-  card: {
+  card:{
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
     "type": "AdaptiveCard",
+    "version": "1.2",
     "body": [
         {
             "type": "TextBlock",
-            "text": "WELLNESS CHECK  - EMPLOYEE CONFIRMATION ",
-            "size": "default",
+            "text": "BUILDING ACCESS",
             "weight": "Bolder"
         },
         {
             "type": "ColumnSet",
-            "style": "default",
             "columns": [
                 {
                     "type": "Column",
@@ -42,7 +42,7 @@ Default.args = {
                     "items": [
                         {
                             "type": "TextBlock",
-                            "text": "Organisational Wellbeing & Resilience",
+                            "text": "Physical Experience",
                             "wrap": true,
                             "size": "Small"
                         }
@@ -56,7 +56,7 @@ Default.args = {
                 },
                 {
                     "type": "Column",
-                    "width": "stretch",
+                    "width": "auto",
                     "items": [
                         {
                             "type": "Image",
@@ -72,66 +72,86 @@ Default.args = {
             "columns": [
                 {
                     "type": "Column",
-                    "width": "auto",
-                    "verticalContentAlignment": "Center",
+                    "width": "stretch",
                     "items": [
                         {
-                            "type": "Image",
-                            "url": "https://icons.iconarchive.com/icons/papirus-team/papirus-status/24/dialog-error-icon.png"
-                        }
-                    ]
-                },
-                {
-                    "type": "Column",
-                    "width": "auto",
-                    "verticalContentAlignment": "Center",
-                    "items": [
+                            "type": "ColumnSet",
+                            "columns": [
+                                {
+                                    "type": "Column",
+                                    "width": "auto",
+                                    "items": [
+                                        {
+                                            "type": "Image",
+                                            "url": "https://icons.iconarchive.com/icons/papirus-team/papirus-status/24/dialog-error-icon.png"
+                                        }
+                                    ],
+                                    "verticalContentAlignment": "Center"
+                                },
+                                {
+                                    "type": "Column",
+                                    "width": "auto",
+                                    "verticalContentAlignment": "Center",
+                                    "backgroundImage": {
+                                        "verticalAlignment": "Center"
+                                    },
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "text": "HIGH IMPORTANCE",
+                                            "wrap": true,
+                                            "color": "Attention",
+                                            "size": "Small"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "type": "Column",
+                                    "width": "auto",
+                                    "separator": true,
+                                    "spacing": "Medium",
+                                    "verticalContentAlignment": "Center"
+                                },
+                                {
+                                    "type": "Column",
+                                    "width": "auto",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "text": "DUE ON:",
+                                            "wrap": true,
+                                            "spacing": "Medium",
+                                            "separator": true,
+                                            "size": "Small",
+                                            "color": "Attention",
+                                            "id": "ac-taglineText2"
+                                        }
+                                    ],
+                                    "verticalContentAlignment": "Center"
+                                },
+                                {
+                                    "type": "Column",
+                                    "width": "auto",
+                                    "items": [
+                                        {
+                                            "type": "TextBlock",
+                                            "text": "10 AUG, 2020",
+                                            "wrap": true,
+                                            "fontType": "Default",
+                                            "size": "Small",
+                                            "color": "Attention"
+                                        }
+                                    ],
+                                    "verticalContentAlignment": "Center"
+                                }
+                            ]
+                        },
                         {
                             "type": "TextBlock",
-                            "text": "HIGH IMPORTANCE",
+                            "text": "I can see you have a meeting in the London office tomorrow.",
                             "wrap": true,
-                            "color": "Attention",
-                            "size": "Small"
-                        }
-                    ]
-                },
-                {
-                    "type": "Column",
-                    "width": "auto",
-                    "spacing": "Medium",
-                    "verticalContentAlignment": "Center",
-                    "separator": true
-                },
-                {
-                    "type": "Column",
-                    "width": "auto",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": "DUE ON:",
-                            "wrap": true,
-                            "spacing": "Medium",
                             "separator": true,
-                            "size": "Small",
-                            "color": "Attention",
-                            "id": "ac-taglineText2"
-                        }
-                    ],
-                    "verticalContentAlignment": "Center"
-                },
-                {
-                    "type": "Column",
-                    "width": "auto",
-                    "verticalContentAlignment": "Center",
-                    "backgroundImage": {},
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": "7 AUG, 2020",
-                            "wrap": true,
-                            "fontType": "Default",
-                            "size": "Small",
-                            "color": "Attention"
+                            "spacing": "Medium"
                         }
                     ]
                 }
@@ -149,208 +169,57 @@ Default.args = {
                     "style": "default",
                     "items": [
                         {
-                            "type": "ColumnSet",
-                            "separator": true,
-                            "columns": [
-                                {
-                                    "type": "Column",
-                                    "width": "stretch",
-                                    "spacing": "None",
-                                    "items": [
-                                        {
-                                            "type": "TextBlock",
-                                            "text": "Check the following statements only if they are true. If they are not true, tell the FSL immediately.",
-                                            "wrap": true,
-                                            "fontType": "Default",
-                                            "spacing": "Large"
-                                        }
-                                    ]
-                                }
-                            ],
-                            "spacing": "Medium"
-                        },
-                        {
                             "type": "TextBlock",
-                            "text": "Would you like to take the wellness check?",
+                            "text": "I can see you have a meeting in the London office tomorrow.",
                             "wrap": true,
                             "fontType": "Default",
                             "spacing": "None"
                         },
                         {
                             "type": "ColumnSet",
+                            "horizontalAlignment": "Right",
                             "columns": [
                                 {
                                     "type": "Column",
-                                    "width": "stretch",
+                                    "width": "90px",
                                     "items": [
                                         {
-                                            "type": "Input.Toggle",
-                                            "title": "I am not sick today and I am heathy to work on this shift",
-                                            "id": "healthy-chckbox1",
-                                            "wrap": true,
-                                            "spacing": "Small"
-                                        },
-                                        {
-                                            "type": "ColumnSet",
-                                            "columns": [
+                                            "type": "ActionSet",
+                                            "actions": [
                                                 {
-                                                    "type": "Column",
-                                                    "width": "stretch",
-                                                    "items": [
-                                                        {
-                                                            "type": "Input.Toggle",
-                                                            "title": "I have not vomitted, had nausea, diarrhea or fever within the last 3 days or i have been cleared to SSR",
-                                                            "id": "healthy-chkbox2",
-                                                            "wrap": true
+                                                    "type": "Action.Submit",
+                                                    "title": "No, its ok",
+                                                    "data": {
+                                                        "answer": "No",
+                                                        "knok": {
+                                                            "removeFromDashboard": true
                                                         }
-                                                    ]
+                                                    }
                                                 }
                                             ]
                                         }
                                     ]
-                                }
-                            ],
-                            "spacing": "Medium"
-                        }
-                    ]
-                }
-            ],
-            "separator": true,
-            "spacing": "Medium"
-        },
-        {
-            "type": "ColumnSet",
-            "columns": [
-                {
-                    "type": "Column",
-                    "width": "stretch",
-                    "items": [
-                        {
-                            "type": "Input.Toggle",
-                            "title": "I do not live with or care for anyone that has committed or had diarrhea today or in the last 3 days before today",
-                            "wrap": true,
-                            "id": "healthy-chkbox3"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "type": "ColumnSet",
-            "columns": [
-                {
-                    "type": "Column",
-                    "width": "stretch",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": "The food safety leader for my shift is:",
-                            "wrap": true,
-                            "isSubtle": true,
-                            "spacing": "Medium"
-                        }
-                    ]
-                }
-            ],
-            "spacing": "Medium"
-        },
-        {
-            "type": "ColumnSet",
-            "columns": [
-                {
-                    "type": "Column",
-                    "width": "stretch",
-                    "items": [
-                        {
-                            "type": "Input.Text",
-                            "placeholder": "Susanne Martha Smith",
-                            "id": "foodSafetyInput"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "type": "ColumnSet",
-            "columns": [
-                {
-                    "type": "Column",
-                    "width": "stretch",
-                    "items": [
-                        {
-                            "type": "TextBlock",
-                            "text": "I checked in at:",
-                            "wrap": true,
-                            "isSubtle": true
-                        }
-                    ]
-                }
-            ],
-            "spacing": "Medium"
-        },
-        {
-            "type": "ColumnSet",
-            "columns": [
-                {
-                    "type": "Column",
-                    "width": "stretch",
-                    "items": [
-                        {
-                            "type": "Input.Text",
-                            "placeholder": "10:25 AM",
-                            "id": "checkedInInput"
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "type": "ColumnSet",
-            "columns": [
-                {
-                    "type": "Column",
-                    "width": "stretch",
-                    "items": [
-                        {
-                            "type": "ColumnSet",
-                            "columns": [
+                                },
                                 {
                                     "type": "Column",
-                                    "width": "stretch",
-                                    "spacing": "Medium",
+                                    "width": "90px",
+                                    "horizontalAlignment": "Right",
                                     "items": [
                                         {
-                                            "type": "Input.Toggle",
-                                            "title": "I certify that the above is true",
-                                            "wrap": true,
-                                            "value": "true",
-                                            "id": "certifyChkBox"
-                                        },
-                                        {
-                                            "type": "ColumnSet",
-                                            "spacing": "Medium",
-                                            "columns": [
+                                            "type": "ActionSet",
+                                            "actions": [
                                                 {
-                                                    "type": "Column",
-                                                    "width": "auto",
-                                                    "items": [
-                                                        {
-                                                            "type": "ActionSet",
-                                                            "actions": [
-                                                                {
-                                                                    "type": "Action.Submit",
-                                                                    "title": "Submit",
-                                                                    "data": {
-                                                                        "answer": "No"
-                                                                    },
-                                                                    "style": "positive"
-                                                                }
-                                                            ]
+                                                    "type": "Action.Submit",
+                                                    "title": "Yes, Please",
+                                                    "data": {
+                                                        "answer": "yes",
+                                                        "knok": {
+                                                            "removeFromDashboard": true
                                                         }
-                                                    ]
+                                                    },
+                                                    "style": "positive"
                                                 }
-                                            ],
-                                            "horizontalAlignment": "Right"
+                                            ]
                                         }
                                     ]
                                 }
@@ -360,13 +229,8 @@ Default.args = {
                 }
             ],
             "spacing": "Medium"
-        },
-        {
-            "type": "ColumnSet"
         }
-    ],
-    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-    "version": "1.2"
+    ]
 },
   themeType : "dark"
   
