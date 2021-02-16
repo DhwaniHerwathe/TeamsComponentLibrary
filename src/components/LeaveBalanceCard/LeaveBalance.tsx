808ca74fb42d6dc5b62afbceb4c37468676f5133
Akhilesh <akhilesh.g.singh>
2021-02-16 16:09:15 +0530

    Fix svg import

--- src/components/LeaveBalanceCard/LeaveBalance.tsx
+++ src/components/LeaveBalanceCard/LeaveBalance.tsx
@@ -6,41 +6,39 @@ import {
   Card,
   Image,
   Text,
-  Button
+  Button,
 } from "@fluentui/react-northstar";
-//import "./StepperForm.css";
+import imageFile from "../../assets/heart-beat.svg";
 
 export const LeaveBalance: React.FC = () => {
- 
-
   return (
     <Provider theme={teamsTheme}>
       <>
         <Flex className="cardWrapper">
-                  <Card className="MyLeaveDetailsCard" aria-roledescription="card with image and text">
-                      <Card.Body fitted>
-                          <Flex className="cardBody" space="between">
-                              <Flex className="leaveTypeValue" column>
-                                  <Image
-                                      src="../../assets/008-heart-beat.svg"
-                                      alt="Sick Leave"
-                                  />
-                                  <Flex>
-                                      <Text color="#484644" size="medium" content="Vacatation" />
-                                  </Flex>
-                              </Flex>
-                              <Flex className="leaveDaysValue">
-                                  <Text content="13" />
-                                  <Text className="daysText" content=" Days" />
-                              </Flex>
-                          </Flex>
-                      </Card.Body>
-                      <Card.Footer fitted>
-                          <Flex className="cardFooter">
-                              <Button className="editButton" content="Request Leave"/>
-                          </Flex>
-                      </Card.Footer>
-                  </Card>
+          <Card
+            className="MyLeaveDetailsCard"
+            aria-roledescription="card with image and text"
+          >
+            <Card.Body fitted>
+              <Flex className="cardBody" space="between">
+                <Flex className="leaveTypeValue" column>
+                  <Image src={imageFile} alt="Sick Leave" />
+                  <Flex>
+                    <Text color="#484644" size="medium" content="Vacatation" />
+                  </Flex>
+                </Flex>
+                <Flex className="leaveDaysValue">
+                  <Text content="13" />
+                  <Text className="daysText" content=" Days" />
+                </Flex>
+              </Flex>
+            </Card.Body>
+            <Card.Footer fitted>
+              <Flex className="cardFooter">
+                <Button className="editButton" content="Request Leave" />
+              </Flex>
+            </Card.Footer>
+          </Card>
         </Flex>
       </>
     </Provider>
