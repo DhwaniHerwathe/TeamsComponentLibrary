62ebb26fcd1ebd7a91335387933cf6f1f9a2ba39
Akhilesh <akhilesh.g.singh>
2021-02-26 15:36:22 +0530

    Fix all linitng issues

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -1,25 +1,20 @@
 import * as React from "react";
-import { Provider, Table, teamsTheme , teamsDarkTheme, teamsHighContrastTheme , ThemeInput} from "@fluentui/react-northstar";
+import { Provider, teamsTheme } from "@fluentui/react-northstar";
 import { Panel, PanelType } from "@fluentui/react/lib/Panel";
 
-export interface ISidePanelProps {
-}
+export interface ISidePanelProps {}
 
-
-
-export const SidePanel: React.FC<ISidePanelProps> = ({
-    ...props
-  }) => {
-    return (
-        <Provider theme={teamsTheme}>
-        <Panel
-            isLightDismiss
-            headerText="Menu"
-            isOpen={true}
-            type={PanelType.smallFixedNear}>
-            <p>Options</p>
-        </Panel>
+export const SidePanel: React.FC<ISidePanelProps> = ({ ...props }) => {
+  return (
+    <Provider theme={teamsTheme}>
+      <Panel
+        isLightDismiss
+        headerText="Menu"
+        isOpen={true}
+        type={PanelType.smallFixedNear}
+      >
+        <p>Options</p>
+      </Panel>
     </Provider>
-    );
+  );
 };
-
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,67 +1,81 @@
 import * as React from "react";
-import { Provider, Table, teamsTheme , teamsDarkTheme, teamsHighContrastTheme , ThemeInput} from "@fluentui/react-northstar";
+import {
+  Provider,
+  Table,
+  teamsTheme,
+  teamsDarkTheme,
+  teamsHighContrastTheme,
+  ThemeInput,
+} from "@fluentui/react-northstar";
 
 /**
  * Type checking for Component State
  */
-type CompState = {
-};
+type CompState = {};
 
-interface TableValues{
-    headers: object;
-    values: object[];
+interface TableValues {
+  headers: object;
+  values: object[];
 }
 type CompProps = {
- 
-    tableData?: TableValues;
-    attributes?: object;
-    theme : object;
-}
+  tableData?: TableValues;
+  attributes?: object;
+  theme: object;
+};
 
 /**
  * Component with a Fluent UI Table and theme changes
  */
 
-export default class TableComponent extends React.Component<CompProps, CompState> {
+export default class TableComponent extends React.Component<
+  CompProps,
+  CompState
+> {
+  constructor(props: CompProps, state: CompState) {
+    super(props);
+    this.state = {};
+  }
 
-    constructor(props: CompProps, state: CompState) {
-        super(props);
-        this.state = {
-        };
-    }
+  render() {
+    const rows: object[] = [];
+    this.props.tableData.values.map((val, idx) => {
+      const key: number = idx + 1;
+      const items: string[] = [];
+      for (let key in val) {
+        items.push(val[key] ? val[key] : "");
+      }
+      const rowObject: object = { key, items };
+      return rows.push(rowObject);
+    });
 
-    render() {
-      const rows: object[] = [];
-      this.props.tableData.values.map((val, idx) => {
-        const key: number = idx + 1;
-        const items: string[] = [];
-        for(let key in val){
-            items.push(val[key] ? val[key] : "");
-        }
-        const rowObject: object = {  key, items };
-        rows.push(rowObject);
-      });
+    // update theme based on props
+    const updateTheme = (themeStr?: string): ThemeInput<any> => {
+      let theme: ThemeInput<any>;
+      switch (themeStr) {
+        case "dark":
+          theme = teamsDarkTheme;
+          break;
+        case "contrast":
+          theme = teamsHighContrastTheme;
+          break;
+        case "light":
+        default:
+          theme = teamsTheme;
+      }
+      return theme;
+    };
 
-      // update theme based on props
-      const updateTheme = (themeStr?: string) : ThemeInput<any> => {
-        let theme: ThemeInput<any>;
-        switch (themeStr) {
-            case "dark":
-                theme = teamsDarkTheme;
-                break;
-            case "contrast":
-                theme = teamsHighContrastTheme;
-                break;
-            case "light":
-            default:
-                theme = teamsTheme;
-        }
-        return theme
-    }; 
-
-    
-      return  (<Provider theme={updateTheme(this.props.theme)}>
-                    <Table variables={this.props.attributes} color="#979593" className="table" header={this.props.tableData.headers} rows={rows} aria-label="Static table" />
-                </Provider>)
-    }
+    return (
+      <Provider theme={updateTheme(this.props.theme)}>
+        <Table
+          variables={this.props.attributes}
+          color="#979593"
+          className="table"
+          header={this.props.tableData.headers}
+          rows={rows}
+          aria-label="Static table"
+        />
+      </Provider>
+    );
+  }
 }
